caec6dfc5a1fa8eec12e12ad55240818db814f66
SHJoon <hojoon0617@gmail.com>
2021-02-24 14:58:14 -0800

    Adding different themes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,40 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
 
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import { orange, green } from "@material-ui/core/colors";
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE688B, #FF8E53)",
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: "white",
+    padding: "0 30px",
+  },
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: orange[500],
+    },
+    secondary: {
+      main: green[400],
+    },
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>;
+}
+
 function CheckboxExample() {
   const [checked, setChecked] = React.useState(true);
 
@@ -32,27 +66,30 @@ function CheckboxExample() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TextField
-          variant="filled"
-          color="secondary"
-          type="email"
-          label="The Time"
-          placeholder="testing@test.com"
-        />
-        <CheckboxExample />
-        <ButtonGroup variant="contained">
-          <Button startIcon={<SaveIcon />} size="large" color="primary">
-            Save
-          </Button>
-          <Button startIcon={<DeleteIcon />} size="large" color="secondary">
-            Discard
-          </Button>
-        </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <ButtonStyled />
+          <TextField
+            variant="filled"
+            color="secondary"
+            type="email"
+            label="The Time"
+            placeholder="testing@test.com"
+          />
+          <CheckboxExample />
+          <ButtonGroup variant="contained">
+            <Button startIcon={<SaveIcon />} size="large" color="primary">
+              Save
+            </Button>
+            <Button startIcon={<DeleteIcon />} size="large" color="secondary">
+              Discard
+            </Button>
+          </ButtonGroup>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
