23c62cc1f38bd22865bdd099a0826d0038f60302
SHJoon <hojoon0617@gmail.com>
2021-02-24 14:35:43 -0800

    Adding checkbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ButtonGroup from "@material-ui/core/BUttonGroup";
 import Button from "@material-ui/core/Button";
-import SaveIcon from '@material-ui/icons/Save'
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{
+            "aria-label": "secondary checkbox",
+          }}
+        />
+      }
+      label="Testing Checkbox"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<SaveIcon />}
-          size="large"
-          style={{
-            fontSize: 24
-          }}
-          variant="contained"
-          color="secondary"
-        >
-          Hello World
-        </Button>
+        <CheckboxExample />
+        <ButtonGroup variant="contained">
+          <Button startIcon={<SaveIcon />} size="large" color="primary">
+            Save
+          </Button>
+          <Button startIcon={<DeleteIcon />} size="large" color="secondary">
+            Discard
+          </Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
