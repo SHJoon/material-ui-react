3fdd91c16c01d2a1ab9edb5a57a62bfb3d47fff7
SHJoon <hojoon0617@gmail.com>
2021-02-24 15:29:28 -0800

    Implemented grid for responsive design

--- src/App.js
+++ src/App.js
@@ -20,6 +20,9 @@ import "fontsource-roboto";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
 const useStyles = makeStyles({
   root: {
     background: "linear-gradient(45deg, #FE688B, #FF8E53)",
@@ -35,7 +38,7 @@ const theme = createMuiTheme({
   typography: {
     h2: {
       fontSize: 18,
-      marginBottom: 15
+      marginBottom: 15,
     },
   },
   palette: {
@@ -77,34 +80,49 @@ function CheckboxExample() {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <Typography variant="h2" component="div">
-            Welcome to MUI
-          </Typography>
-          <Typography variant="subtitle1">
-            Learn how to use Material Ui
-          </Typography>
-          <ButtonStyled />
-          <TextField
-            variant="filled"
-            color="secondary"
-            type="email"
-            label="The Time"
-            placeholder="testing@test.com"
-          />
-          <CheckboxExample />
-          <ButtonGroup variant="contained">
-            <Button startIcon={<SaveIcon />} size="large" color="primary">
-              Save
-            </Button>
-            <Button startIcon={<DeleteIcon />} size="large" color="secondary">
-              Discard
-            </Button>
-          </ButtonGroup>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <Container maxWidth="xl">
+        <div className="App">
+          <header className="App-header">
+            <Typography variant="h2" component="div">
+              Welcome to MUI
+            </Typography>
+            <Typography variant="subtitle1">
+              Learn how to use Material Ui
+            </Typography>
+            <ButtonStyled />
+
+            {/* <TextField
+              variant="filled"
+              color="secondary"
+              type="email"
+              label="The Time"
+              placeholder="testing@test.com"
+            /> */}
+            <Grid container spacing={2} justify="center">
+              <Grid item xs={12} sm={6}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs={3} lg={12}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+            </Grid>
+
+            <CheckboxExample />
+            <ButtonGroup variant="contained">
+              <Button startIcon={<SaveIcon />} size="large" color="primary">
+                Save
+              </Button>
+              <Button startIcon={<DeleteIcon />} size="large" color="secondary">
+                Discard
+              </Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
     </ThemeProvider>
   );
 }
